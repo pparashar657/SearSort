deeaa5bf4664cfbfaf957b425f120767c37a025c
pawan <pparashar657@gmail.com>
2017-12-07 15:11:37 +0530

    third

---
+++ app/src/main/java/com/example/pawan/sortingandsearching/SplashScreen.java
@@ -0,0 +1,30 @@
+package com.example.pawan.sortingandsearching;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        Thread t = new Thread(){
+            @Override
+            public void run() {
+                try {
+                    sleep(1000);
+                    Intent i = new Intent(getApplicationContext(),MainActivity.class);
+                    startActivity(i);
+                    finish();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+
+        t.start();
+    }
+}
